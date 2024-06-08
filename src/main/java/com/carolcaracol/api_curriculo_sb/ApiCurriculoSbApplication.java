573c2b0ea4cb14760ce708c38daf916e6873f049
carolcaracol <ana.00000845333@unicap.br>
2024-06-08 19:03:09 -0300

    commit inicial

---
+++ src/main/java/com/carolcaracol/api_curriculo_sb/ApiCurriculoSbApplication.java
@@ -0,0 +1,13 @@
+package com.carolcaracol.api_curriculo_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCurriculoSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCurriculoSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carolcaracol/api_curriculo_sb/ApiCurriculoSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carolcaracol.api_curriculo_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCurriculoSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
